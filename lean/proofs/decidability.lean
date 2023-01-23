b3d4d9dd07c87e39504be7793d730958a05456ca
Luka Janjić <luka.janjic@gmail.com>
2023-01-23 04:55:19 +0100

    reintroduce the decidable eq of val proof

--- lean/proofs/decidability.lean
+++ lean/proofs/decidability.lean
@@ -2,6 +2,17 @@ import ..compiler ..semantics ..lovelib
 
 open bound val
 
+instance decidable_val_eq : decidable_eq val
+| (VNat n) (VNat m) := dite (n = m) 
+  (λ h, is_true $ congr_arg VNat h)
+  (λ h, is_false $ assume heq, by cases' heq; contradiction)
+| (VBool x) (VBool y) := dite (x = y) 
+  (λ h, is_true $ congr_arg VBool h) 
+  (λ h, is_false $ assume heq, by cases' heq; contradiction)
+| (VNat _) (VBool _) := is_false (assume heq, by contradiction)
+| (VBool _) (VNat _) := is_false (assume heq, by contradiction)
+
+
 def decide_exists_bound_val 
   (x : string) (nv : list (string × val)) 
   : psum (∃v, bound x v nv) (¬ ∃v, bound x v nv) :=
